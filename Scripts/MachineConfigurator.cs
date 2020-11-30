b3fff49390d8cd851295912bae7b665fe6d5772d
Quwiyu <68051831+Ajibade-Quwiyu@users.noreply.github.com>
2020-11-30 22:12:46 +0800

    Add files via upload

---
+++ Scripts/MachineConfigurator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MachineConfigurator : MonoBehaviour
+{
+    [SerializeField] private Slider rotator;
+    [SerializeField] private Slider scaler;
+    [SerializeField] private Toggle exploder;
+    private Canvas rend;
+    private Animator anim;
+
+
+
+    private void Awake()
+    {
+        rend = GetComponentInChildren<Canvas>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    public void MachineRotate () 
+    {
+        transform.rotation = Quaternion.Euler(0, rotator.value, 0);
+    }
+
+    public void MachineScale()
+    {
+        transform.localScale = new Vector3(scaler.value, scaler.value, scaler.value);
+    }
+    public void OnObject()
+    {
+       
+        if (rend.enabled)
+            rend.enabled = false;
+        else
+            rend.enabled = true;
+    }
+    public void OnExplode()
+    {
+        if (!exploder.isOn)
+        {
+            anim.Play("in");
+        }
+        else
+            anim.Play("out");
+    }
+   
+}
---
+++ Scripts/MachineController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineController : MonoBehaviour
+{
+    public GameObject[] Machines;
+
+    private void Start()
+    {
+        ShowObject(0);
+    }
+
+    public void ShowObject(int Obj)
+    {
+        int Count = Machines.Length;
+
+        if (Obj <= Count)
+        {
+            for(int i = 1; i < Count+1; i++)
+            {
+                if (Obj == i)
+                {
+                    Machines[i - 1].SetActive(true);
+                }
+                else
+                {
+                    Machines[i - 1].SetActive(false);
+                }
+            }
+        }
+    }
+     
+}
